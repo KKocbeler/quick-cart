3e056bc513c1a49da3d839048f3a248cb7b13ea9
Kemal Koçbeler <kkocbeler@gmail.com>
2025-03-05 20:44:22 +0300

    General Css codes

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 
 const MainLayout = () => {
-  return (
-    <div>
-        Navbar
-        <Outlet />
-    </div>
-  )
+    return (
+        <>
+            Navbar
+            <div className="container">
+                <Outlet />
+            </div>
+        </>
+    )
 }
 
 export default MainLayout
