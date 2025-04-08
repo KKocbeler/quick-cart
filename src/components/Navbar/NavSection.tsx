b134f9333810af3d114e7adea0eb29d002ea0495
Kemal Koçbeler <kkocbeler@gmail.com>
2025-04-08 23:40:49 +0300

    Changing section titles

--- src/components/Navbar/NavSection.tsx
+++ src/components/Navbar/NavSection.tsx
@@ -1,146 +1,20 @@
-import { useState } from 'react';
 import './NavSection.scss';
-import { Link } from "react-router-dom";
-
-interface SecProps {
-    id: number;
-    name: string;
-    isOver: boolean;
-}
-
-interface ListProps {
-    [key: string]: { title: string, listItems: string[] }[];
-}
+import { Link } from 'react-router-dom';
+import { FixLinkText } from '../../utility/FixLinkText';
 
 const NavSection = () => {
-    const [sections, setSections] = useState<SecProps[]>([
-        { id: 1, name: "woman", isOver: false },
-        { id: 2, name: "male", isOver: false },
-        { id: 3, name: "child", isOver: false },
-        { id: 4, name: "outlet", isOver: false },
-    ]);
-
-    const list: ListProps = {
-        woman: [
-            {
-                title: 'New Arrivals',
-                listItems: ["See all products"]
-            },
-            {
-                title: 'Top Wear',
-                listItems: ['Athlete', 'T-shirt', 'Shirt', 'Blouse', 'Dress', 'Bustier', 'Sweatshirt', 'Jacket']
-            },
-            {
-                title: 'Bottom Wear',
-                listItems: ['Jeans', 'Skirt', 'Shorts', 'Trousers']
-            },
-            {
-                title: 'Accessories',
-                listItems: ['Bags', 'Jewelry', 'Hats', 'Scarves']
-            }
-        ],
-        male: [
-            {
-                title: 'New Arrivals',
-                listItems: ["See all products"]
-            },
-            {
-                title: 'Top Wear',
-                listItems: ['T-shirt', 'Shirt', 'Sweater', 'Jacket']
-            },
-            {
-                title: 'Bottom Wear',
-                listItems: ['Jeans', 'Shorts', 'Trousers']
-            },
-            {
-                title: 'Accessories',
-                listItems: ['Bags', 'Watches', 'Belts', 'Hats']
-            }
-        ],
-        child: [
-            {
-                title: 'New Arrivals',
-                listItems: ["See all products"]
-            },
-            {
-                title: 'Top Wear',
-                listItems: ['T-shirt', 'Sweatshirt', 'Jacket']
-            },
-            {
-                title: 'Bottom Wear',
-                listItems: ['Shorts', 'Trousers', 'Skirt']
-            },
-            {
-                title: 'Accessories',
-                listItems: ['Bags', 'Hats', 'Gloves']
-            }
-        ],
-        outlet: [
-            {
-                title: 'New Arrivals',
-                listItems: ["See all products"]
-            },
-            {
-                title: 'Top Wear',
-                listItems: ['Discounted T-shirts', 'Jackets', 'Sweaters']
-            },
-            {
-                title: 'Bottom Wear',
-                listItems: ['Jeans', 'Shorts', 'Trousers']
-            },
-            {
-                title: 'Accessories',
-                listItems: ['Bags', 'Jewelry', 'Watches']
-            }
-        ]
-    };
-    
-
-    const menuElementOpen = (id: number) => {
-        setSections((prevSection) => 
-            prevSection.map((item) => (
-                item.id === id ? {...item, isOver: true} : item
-            ))
-        )
-    }
-
-    const menuElementClose = (id: number) =>{
-        setSections((prevSection) => 
-            prevSection.map((item) => item.id === id ? {...item, isOver: false} : item)
-        )
-    }
+    const mostSearched = ["Mouse","Smart Watch", "Smart TV", "Latop", "Sunglasses", "Sneakers", "Table", "Lamp", "Shoes", "Shampoo", "Headphone"]
   return (
-    <div className="nav-section">
-        <ul className='section-list'>
+    <div className='nav-section'>
+        <div className="categories">
             {
-                sections?.map((item) => (
-                    <li key={item.id} onMouseOver={() => menuElementOpen(item.id)} onMouseOut={() => menuElementClose(item.id)}>
-                        <Link to={'#'} >{item.name}</Link>
-                        <div className={`dropdown-menu ${item.isOver ? 'show' : ''}`}>
-                            {
-                                list?.[item.name]?.map((dt, i) => (
-                                    <div className='dropdown-menu__list' key={dt.title + i}>
-                                        <h4>{dt.title}</h4>
-                                        <ul>
-                                            {
-                                                dt.listItems.map((bt, j) => (
-                                                    <li key={bt + j}>
-                                                        {bt}
-                                                    </li>
-                                                ))
-                                            }
-                                        </ul>
-                                    </div>
-                                ))
-                            }
-                            <div className='dropdown-menu__img'>
-                                <img src="src/assets/deneme.webp" alt="" />
-                            </div>
-                        </div>
-                    </li>
+                mostSearched.map((item, i) => (
+                    <div className="category" key={i}>
+                        <Link to={`/search/${FixLinkText(item)}`}>{item}</Link>
+                    </div>
                 ))
             }
-        </ul>
+        </div>
     </div>
   )
 }
