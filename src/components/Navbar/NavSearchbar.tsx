d17cc6af25833461889ae40277a55042ec00e48a
Kemal Koçbeler <kkocbeler@gmail.com>
2025-04-08 23:37:05 +0300

    Nav searchbar adjustment

--- src/components/Navbar/NavSearchbar.tsx
+++ src/components/Navbar/NavSearchbar.tsx
@@ -1,12 +1,23 @@
 import { CiSearch } from "react-icons/ci";
 import './NavSearchbar.scss';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FixLinkText } from "../../utility/FixLinkText";
 
 const NavSearchbar = () => {
+    const [keyword, setKeyword] = useState("");
+    const navigate = useNavigate();
+    const SearchedWord = (e: React.FormEvent) => {
+        e.preventDefault();
+        navigate(`/search/${FixLinkText(keyword)}`)
+        setKeyword("");
+    }
+
     return (
         <div className="nav-searchbar">
-            <form>
-                <input type="text" placeholder="Search..." />
-                <span> <CiSearch /> </span>
+            <form onSubmit={(e) => SearchedWord(e)}>
+                <input type="text" placeholder="Search..." value={keyword} onChange={(e) => setKeyword(e.target.value)}/>
+                <button type="submit" aria-label="Search"> <CiSearch /> </button>
             </form>
         </div>
     )
