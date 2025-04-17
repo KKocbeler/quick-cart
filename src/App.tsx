2ab9777757a305f186d701a7729da19f0aa6b840
Kemal Koçbeler <kkocbeler@gmail.com>
2025-04-17 12:32:38 +0300

    add main page routes and update global styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.scss'
 import Home from './page/Home'
 import MainLayout from './layouts/MainLayout'
+import ProductDetails from './page/ProductDetails'
+import { ScrollToTop } from './utility/ScrollToTop'
+import SearchPage from './components/Search/SearchPage'
+import Cart from './page/Cart'
+import Favorite from './page/Favorite'
+import NotFoundPage from './components/Pieces/NotFoundPage'
+import MyAccount from './page/MyAccount'
 
 function App() {
 
     return (
         <Router>
+            <ScrollToTop />
             <Routes>
                 <Route element={<MainLayout />}>
                     <Route path='/' element={<Home />} />
+                    <Route path=':category/:name' element={<ProductDetails />} />
+                    <Route path='/search' element={<SearchPage /> } />
+                    <Route path='/search/:name' element={<SearchPage />} />
+                    <Route path='/cart' element={<Cart /> } />
+                    <Route path='/my-account' element={<MyAccount />} />
+                    <Route path='/my-favorite' element={<Favorite /> } />
+                    <Route path='*' element={<NotFoundPage />} />
                 </Route>
             </Routes>
         </Router>
