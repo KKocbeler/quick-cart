e4bdb42356c00841477b9f6672f19831cedf53a6
Kemal Koçbeler <kkocbeler@gmail.com>
2025-04-25 13:42:16 +0300

    Fixed URLs in MyAccountPage.tsx with useParams

--- src/components/MyAccount/MyAccountPage.tsx
+++ src/components/MyAccount/MyAccountPage.tsx
@@ -3,47 +3,57 @@ import './MyAcccontPage.scss';
 import { CiGift, CiHeart, CiLogout, CiSettings, CiShoppingCart } from 'react-icons/ci';
 import { PiAddressBookLight } from 'react-icons/pi';
 import { GoChevronRight } from 'react-icons/go';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import FavoritePage from '../Favorite/FavoritePage';
 import MyAddresses from './MyAccountPage/MyAddresses';
+import { useNavigate, useParams } from 'react-router-dom';
+import { FixLinkText } from '../../utility/FixLinkText';
+
 
 const MyAccountPage = () => {
     const [showTab, setShowTab] = useState("My Addresses");
+    const navigate = useNavigate();
+    const { tab } = useParams();
+
+    useEffect(() => {
+      if (tab) {
+        const found = accountList.find(item => FixLinkText(item.title) === tab)
+        if (found) {
+          setShowTab(found.title)
+        }
+      }
+    }, [tab])
+
     const accountList = [
         {
           icon: <PiAddressBookLight />,
           title: "My Addresses",
-          path: "/account/addresses",
         },
         {
           icon: <CiShoppingCart />,
           title: "My Orders",
-          path: "/account/orders",
         },
         {
           icon: <CiHeart />,
           title: "My Favorites",
-          path: "/account/favorites",
         },
         {
           icon: <CiSettings />,
-          title: "Account Settings",
-          path: "/account/settings",
+          title: "Settings",
         },
         {
           icon: <CiGift />,
-          title: "My Points & Vouchers",
-          path: "/account/vouchers",
+          title: "My Points",
         },
         {
           icon: <CiLogout />,
           title: "Logout",
-          path: "/logout",
         },
     ];
 
-    const handleTab = (title: string) => {
+    const handleTab = (title: string, tab: string) => {
         setShowTab(title)
+        navigate(`/my-account/${FixLinkText(tab)}`)
     };
 
   return (
@@ -57,7 +67,7 @@ const MyAccountPage = () => {
             <ul className='account-list'>
                 {
                     accountList.map((item, index) => (
-                        <li key={index} onClick={() => handleTab(item.title)}>
+                        <li key={index} onClick={() => handleTab(item.title, item.title)}>
                             <div>
                                 {item.icon} <span>{item.title}</span>
                             </div>
