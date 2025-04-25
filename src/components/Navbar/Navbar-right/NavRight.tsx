6c5e795683c519b4f96aa263b0d554a723d49cbe
Kemal Koçbeler <kkocbeler@gmail.com>
2025-04-25 13:43:12 +0300

    Added Account component

--- src/components/Navbar/Navbar-right/NavRight.tsx
+++ src/components/Navbar/Navbar-right/NavRight.tsx
@@ -1,11 +1,12 @@
 import './NavRight.scss';
 import NavCart from "./Nav-right-inner/NavCart";
-import { CiHeart, CiUser } from 'react-icons/ci';
+import { CiHeart } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
+import Account from './Nav-right-inner/Account';
 const NavRight = () => {
   return (
     <ul className="nav-right">
-        <li><Link to={"my-account"}><CiUser /> <span>My Account</span></Link></li>
+        <li> <Account /> </li>
         <li><Link to={"/my-favorite"}> <CiHeart /> <span>My Favorite</span> </Link> </li>
         <li><NavCart /></li>
     </ul>
